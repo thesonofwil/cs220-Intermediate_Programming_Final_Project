8e4ec6cb7699792a6570dd4bf569c98d8f9e2ad4
Brhea D'Mello <brheadmello@gmail.com>
2020-04-22 15:26:32 -0400

    implement wall.cpp

---
+++ wall.cpp
@@ -0,0 +1,24 @@
+//Brhea D'Mello
+//bdmello2
+
+#include "wall.h"
+
+
+Wall::Wall(){
+}
+
+Wall::~Wall(){
+}
+
+MoveResult Wall::checkMoveOnto(Entity *entity, const Position &fromPos, const Position &tilePos) const {
+  return MoveResult::Allow;
+}
+
+bool Wall::isGoal() const {
+	return false;
+}
+
+std::string Wall::getGlyph() const {
+	return "#";
+}
+
