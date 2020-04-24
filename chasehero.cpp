f0a92c338c09cd54433d3a847c3d8fdc9ce17265
Jingyu <jhuan146@jhu.edu>
2020-04-24 18:25:12 -0400

    chasehero.cpp edited

---
+++ chasehero.cpp
@@ -0,0 +1,18 @@
+//Jingyu Huang
+//jhuan146
+
+#include "chasehero.h"
+
+ChaseHero::ChaseHero{
+}
+
+ChaseHero::~ChaseHero{
+}
+
+Direction ChaseHero::getMoveDirection(Game *game, Entity *entity){
+  
+}
+
+bool ChaseHero::isUser() const{
+  return false;
+}
