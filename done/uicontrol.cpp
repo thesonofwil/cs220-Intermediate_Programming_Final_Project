6579b6d4322e360df14d6abff4e6b80cacf32c12
Jingyu <jhuan146@jhu.edu>
2020-04-26 13:44:19 -0400

    folder implemented

---
+++ done/uicontrol.cpp
@@ -0,0 +1,22 @@
+//jhuan146
+//Jingyu Huang
+
+#include "uicontrol.h"
+
+UIControl::UIControl(){
+}
+
+UIControl::~UIControl(){
+}
+
+//Get suggested move from UI object
+Direction UIControl::getMoveDirection(Game *game, Entity *entity){
+  UI *ui = UI::getInstance();
+  Direction dir = ui->getMoveDirection();
+  return dir;
+}
+
+//EntityController is controlled by use input
+bool UIControl::isUser() const{
+  return true;
+}
