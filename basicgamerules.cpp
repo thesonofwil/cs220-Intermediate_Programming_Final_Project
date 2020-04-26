f7ef8e05cb01435aefb9cc516e7c72011cd20577
Wilson Tjoeng <wtjoeng1@jhu.edu>
2020-04-26 14:39:32 -0400

    Started BasicGameRules

---
+++ basicgamerules.cpp
@@ -0,0 +1,19 @@
+// Wilson Tjoeng
+// wtjoeng1
+
+#include "basicgamerules.h"
+
+BasicGameRules::BasicGameRules() {
+}
+
+BasicGameRules::~BasicGameRules() {
+}
+
+bool BasicGameRules::allowMove(Game *game, Entity *actor, const Position &source, const Position &dest) const {
+}
+
+void BasicGameRules::enactMove(Game *game, Entity *actor, const Position &dest) const {
+}
+
+GameResult checkGameResult(Game *game) const {
+}
