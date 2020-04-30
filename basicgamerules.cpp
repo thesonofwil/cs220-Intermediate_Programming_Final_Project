ddc081f9baf65bb1e47e6831980fc62095a10aa4
Wilson Tjoeng <wtjoeng1@jhu.edu>
2020-04-30 16:27:46 -0400

    No more compiler issues

---
+++ basicgamerules.cpp
@@ -0,0 +1,146 @@
+// Wilson Tjoeng
+// wtjoeng1
+
+#include "basicgamerules.h"
+#include "entity.h"
+#include "game.h"
+#include "maze.h"
+#include "tile.h"
+
+BasicGameRules::BasicGameRules() {
+}
+
+BasicGameRules::~BasicGameRules() {
+}
+
+/* Allow Entity to make a move if:
+ 1. it is onto an adjacent unnocupied Tile, and the Tile's checkMoveOnto member function allows the move, or
+
+ 2. it is onto an adjacent Tile occupied by an entity with the “v” (moveable) property, and the moveable entity is permitted to move onto an unoccupied adjacent Tile in the same direction that the original entity is moving. This case allows the entity to "push" a moveable entity.
+
+Moves out of bounds, or moves by more than 1 unit of distance, are not allowed.
+*/
+bool BasicGameRules::allowMove(Game *game, Entity *actor, const Position &source, const Position &dest) const {
+
+  // Return false if inanimate object, move would be > 1 space, or if dest contains a moveable object
+  // that can't move further
+  if (actor->hasProperty('v') || source.distanceFrom(dest) > 1 || !checkObjCanBePushed(game, actor, dest)) {
+    return false;           
+  }
+
+  // Check if actor can move to dest tile.
+  Maze *maze = game->getMaze();
+  const Tile *tile = maze->getTile(dest); 
+  MoveResult result = tile->checkMoveOnto(actor, source, dest);
+  if (result == MoveResult::ALLOW) {
+    return true;
+  }
+  
+  return false;
+}
+
+// Update the positions of the entity and the inanimate object if pushed
+// Precondition: allowMove() returned true
+void BasicGameRules::enactMove(Game *game, Entity *actor, const Position &dest) const {
+
+  // Check if any pushable objects occupy the dest tile.
+  std::vector<Entity *> objs = game->getEntitiesWithProperty('v');
+  for (int i = 0; i < (int) objs.size(); i++) {
+    if (objs[i]->hasProperty('v')) {
+      Direction dir = getPushDirection(actor, objs[i]);
+      Position newPos = getPushPosition(game, dir, objs[i]);
+      objs[i]->setPosition(newPos);
+      break; // Only one object at a tile at a time
+    }
+  }
+  
+  actor->setPosition(dest);
+}
+
+// Return the current game state.
+// If hero reaches Goal, return GameResult::HERO_WINS.
+// If minotour reaches hero, return GameResult::HERO_LOSES.
+// Otherwise return GameResult::UNKNOWN.
+// GameResult defined in gamerules.h
+GameResult BasicGameRules::checkGameResult(Game *game) const {
+  Maze *maze = game->getMaze();
+  std::vector<Entity *> heroes = game->getEntitiesWithProperty('h'); // Get vector of heroes. A game may have multiple heroes
+  std::vector<Entity *> minotaurs = game->getEntitiesWithProperty('m'); // Get vector of minotaurs.
+  // Get list of positions of minotaurs
+  std::vector<Position> minPositions;
+  if (!minotaurs.empty()) {
+    for (Entity* minotaur : minotaurs) {
+      minPositions.push_back(minotaur->getPosition());
+    }
+  }
+  
+  for (Entity* hero : heroes) {
+    Position pos = hero->getPosition();
+    const Tile *tile = maze->getTile(pos);
+    if (tile->isGoal()) {
+      return GameResult::HERO_WINS;
+    }
+    // Check if any minotaur is at the same position as hero
+    if (!minPositions.empty()) {
+      for (Position p : minPositions) {
+	if (p == pos) {
+	  return GameResult::HERO_LOSES;
+	}
+      }
+    }
+  }
+  return GameResult::UNKNOWN;
+}
+
+// Get the direction of the actor pushing onto an inanimate object. 
+Direction BasicGameRules::getPushDirection(Entity *actor, Entity *obj) const {
+  Position actorPos = actor->getPosition(); // Get position of object
+  Position objPos = obj->getPosition(); // Get position if it were displaced
+
+  int x = objPos.getX() - actorPos.getX();
+  int y = objPos.getY() - actorPos.getY();
+
+  if (x == 0 && y == 1) {
+    return Direction::UP;
+  } else if (x == 0 && y == -1) {
+    return Direction::DOWN;
+  } else if (x == 1 && y == 0) {
+    return Direction::RIGHT;
+  } else if (x == -1 && y == 0) {
+    return Direction::LEFT;
+  }
+  return Direction::NONE; // Invalid push
+}
+
+// Get the new position of an object when it's pushed
+Position BasicGameRules::getPushPosition(Game *game, Direction dir, Entity *obj) const {
+  Maze *maze = game->getMaze();
+  Position newPos = obj->getPosition().displace(dir); // Get new position if object were to be pushed
+  const Tile *destTile = maze->getTile(newPos); // Get tile at destination
+  MoveResult check = destTile->checkMoveOnto(obj, obj->getPosition(), newPos);
+  
+  // Check if newPos is valid
+  if (!newPos.inBounds(maze->getWidth(), maze->getHeight()) || check == MoveResult::BLOCK) {
+    return obj->getPosition(); // No change in position
+  }
+  return newPos; 
+}
+
+bool BasicGameRules::checkObjCanBePushed(Game *game, Entity *actor, const Position &dest) const {
+  Entity *object = game->getEntityAt(dest);
+  if (object->hasProperty('v')) { // v denotes moveable entity
+
+    // Check if moveable object can be moved further
+    Direction dir = getPushDirection(actor, object);
+    if (dir == Direction::NONE) {
+      return false;
+    }
+    
+    Position pushPos = getPushPosition(game, dir, object);
+    if (pushPos == object->getPosition()) { // Object cannot be pushed
+      return false;
+    }
+    return true;
+  }
+  return true; // Default: there is no object
+}
---
+++ basicgamerules.h
@@ -0,0 +1,28 @@
+#ifndef BASICGAMERULES_H
+#define BASICGAMERULES_H
+
+#include "gamerules.h"
+#include "position.h"
+
+class BasicGameRules : public GameRules {
+private:
+  // copy constructor and assignment operator are disallowed
+  BasicGameRules(const BasicGameRules &);
+  BasicGameRules &operator=(const BasicGameRules &);
+
+public:
+  BasicGameRules();
+  virtual ~BasicGameRules();
+
+  virtual bool allowMove(Game *game, Entity *actor, const Position &source, const Position &dest) const;
+  virtual void enactMove(Game *game, Entity *actor, const Position &dest) const;
+  virtual GameResult checkGameResult(Game *game) const;
+
+private:
+  // add your own private member functions...
+  virtual Direction getPushDirection(Entity *actor, Entity *obj) const;
+  virtual Position getPushPosition(Game *game, Direction dir, Entity *obj) const;
+  virtual bool checkObjCanBePushed(Game *game, Entity *actor, const Position &dest) const;
+};
+
+#endif // BASICGAMERULES_H
