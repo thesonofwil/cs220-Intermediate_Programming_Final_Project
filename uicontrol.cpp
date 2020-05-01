8e4f436f3c7564dead698ade81bbaae7979a4285
Jingyu <jhuan146@jhu.edu>
2020-04-30 20:05:46 -0400

    uicontrol files added

---
+++ uicontrol.cpp
@@ -0,0 +1,20 @@
+//jhuan146
+//Jingyu Huang
+
+#include "uicontrol.h"
+
+UIControl::UIControl(){
+}
+
+UIControl::~UIControl(){
+}
+
+Direction UIControl::getMoveDirection(Game, Entity){
+  UI *ui = UI::getInstance();
+  Direction dir = ui->getMoveDirection();
+  return dir;
+}
+
+bool UIControl::isUser const{
+  return false;
+}
