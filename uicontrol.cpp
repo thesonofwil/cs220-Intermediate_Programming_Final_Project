53a646e29404b666b721dd80af39eb7970a51927
Jingyu Huang <jhuan146@jhu.edu>
2020-05-01 20:22:17 +0000

    uicontrol.cpp edited online with Bitbucket

--- uicontrol.cpp
+++ uicontrol.cpp
@@ -9,12 +9,14 @@ UIControl::UIControl(){
 UIControl::~UIControl(){
 }
 
+//Get suggested move from UI object
 Direction UIControl::getMoveDirection(Game, Entity){
   UI *ui = UI::getInstance();
   Direction dir = ui->getMoveDirection();
   return dir;
 }
 
+//EntityController is controlled by use input
 bool UIControl::isUser() const{
-  return false;
+  return true;
 }
