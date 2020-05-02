a6f86f2e039c6eb65d6dfe000f2ca541e8a36ff3
Brhea D'Mello <brheadmello@gmail.com>
2020-05-02 19:24:18 -0400

    edit fix compiler

--- astarchasehero.cpp
+++ astarchasehero.cpp
@@ -1,2 +1,18 @@
-//bdmello2
-//
+ //Brhea D'Mello
+ //bdmello2
+ 
+#include "astarchasehero.h"
+ 
+ AStarChaseHero ::AStarChaseHero(){
+ }
+ 
+ AStarChaseHero::~AStarChaseHero(){
+ }
+ 
+ Direction AStarChaseHero::getMoveDirection(Game *game, Entity *entity) {
+ //  return Direction::UP;
+ }
+ 
+ bool AStarChaseHero::isUser() const {
+         return false;
+ }
