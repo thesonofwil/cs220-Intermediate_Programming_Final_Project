62252264d4b825dfa29bfc186a978f1829157d99
Wilson Tjoeng <wtjoeng1@jhu.edu>
2020-05-03 19:39:09 -0400

    Minor updates

--- game.cpp
+++ game.cpp
@@ -121,6 +121,7 @@ void Game::gameLoop() {
       // Check if game over condition has been met
       GameResult status = this->gameRules->checkGameResult(this);
       if (status == GameResult::HERO_WINS) {
+	std::cout << "Win" << std::endl;
 	this->ui->displayMessage("Hero wins", true); // endgame = true
 	gameOver = true;
 	break; // Exit out of for and while loops
