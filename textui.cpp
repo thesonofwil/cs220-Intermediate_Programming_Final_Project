959e347c363264c2cb387d1950167e3bc78b05cf
Jingyu <jhuan146@jhu.edu>
2020-05-03 19:01:34 -0400

    render function implemented

--- textui.cpp
+++ textui.cpp
@@ -21,11 +21,13 @@ TextUI::~TextUI(){
 
 Direction TextUI::getMoveDirection(){
   std::cout << "Your move (u/d/l/r): " << std::endl;
-  char input;
-  std::cin >> input;
+  std::string inputline;
+  std::cin >> inputline;
+  char input = inputline.at(0);
   while((input!='u') && (input!='d') && (input!='l') && (input!='r')){
     std::cout << "Unknown direction" <<	std::endl;
-    std::cin >> input;
+    std::cin >> inputline;
+    input = inputline.at(0);
   }
   switch(input){
     case 'u':
@@ -63,6 +65,6 @@ void TextUI::render(Game *game){
   }
   if (this->message != ""){
     std::cout << this->message << ": " << std::endl;
-    delete this->message;
+    (this->message).erase();
   }
 }
